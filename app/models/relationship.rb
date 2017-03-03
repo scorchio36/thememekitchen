ad9a6366b229394037f1370ae14f82fb844ce173
checker18 <frizzleskillet@yahoo.com>
2017-03-03 17:47:19 -0500

    Add following capablities

---
+++ app/models/relationship.rb
@@ -0,0 +1,8 @@
+class Relationship < ApplicationRecord
+
+  #followed is the person in the relationship being followed
+  #follower is the person in the relationship doing the following
+  belongs_to :follower, class_name: "User" #Ex: if I said active_relationship.follower its like a different version of saying Relationship.User
+  belongs_to :followed, class_name: "User",
+
+end
