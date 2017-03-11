51668d14ef72e5f8bec0ad401aba66dfeec0fafc
checker18 <frizzleskillet@yahoo.com>
2017-03-10 22:13:32 -0500

    Attempt to fix error with notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
 
   def notification_list
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
     @notifications = @user.notifications
   end
 end
